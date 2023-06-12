46d22112dfde8290c8cb2bcbea18cccfd3ddd62d
Olatoyan <golatoyan@gmail.com>
2023-06-12 20:33:22 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const adviceNum = document.querySelector(".advice__number");
+const adviceText = document.querySelector(".advice__text");
+const rollDice = document.querySelector(".img__box");
+
+const getAdvice = async function () {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  console.log(data.slip);
+
+  adviceNum.textContent = data.slip.id;
+  adviceText.textContent = data.slip.advice;
+};
+
+getAdvice();
+
+rollDice.addEventListener("click", getAdvice);
